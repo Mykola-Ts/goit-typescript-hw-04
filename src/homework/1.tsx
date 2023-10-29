918a19f671abe7f767d2150a44a919d902a7e7b0
Mykola Ts <tsybulskiyk@gmail.com>
2023-10-29 20:21:13 +0200

    completed tasks

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -5,19 +5,11 @@ type Props = {
   onContentEndVisible(): void;
 };
 
-class Options {
-  constructor(
-    public rootMargin: string,
-    public threshold: number,
-    public root: HTMLElement | null
-  ) {}
-}
-
 export function Observer({ children, onContentEndVisible }: Props) {
   const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const options: Options = {
+    const options: IntersectionObserverInit = {
       rootMargin: "0px",
       threshold: 1.0,
       root: null,
